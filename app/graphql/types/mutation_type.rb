2ca66431b50161027269ea0645f4c6bc5ab165a9
viveckadhriyaa <viveck0@gmail.com>
2020-04-22 12:13:30 +0530

    feat: Auth logout removed from base mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,7 +3,6 @@ module Types
 
     field :auth_sign_up,                  resolver: Mutations::Auth::SignUp
     field :auth_login,                    resolver: Mutations::Auth::Login
-    field :auth_logout,                   resolver: Mutations::Auth::Logout
-    
+
   end
 end
