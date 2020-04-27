62c71a4020215ab6e9f1372af7c8aa9ab0b4492d
viveckadhriyaa <viveck0@gmail.com>
2020-04-27 12:20:00 +0530

    Post create table mutation added

--- db/migrate/20200427050443_create_posts.rb
+++ db/migrate/20200427050443_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.references :user, null: false, foreign_key: true
       t.text :body
-      t.integer :type
+      t.integer :post_type
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_050443) do
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.text "body"
-    t.integer "type"
+    t.integer "post_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
