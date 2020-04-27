8025370729ccadb5e6f9e43b8feecdb58bf64901
viveckadhriyaa <viveck0@gmail.com>
2020-04-27 13:01:00 +0530

    Post listing added

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,8 @@
 module Types
   class QueryType < Types::BaseObject
-    field :get_post,        resolver: Queries::Post::GetPost
+
+    field :get_post,          resolver: Queries::Post::GetPost
+    field :list_posts,        resolver: Queries::Post::ListPosts
+
   end
 end
