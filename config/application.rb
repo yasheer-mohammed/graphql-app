61e27a58e9a4f110ad81410c438e03ef7f301ebe
yasheer-mohammed <yasheeer.yash03@gmail.com>
2024-06-01 14:27:24 +0530

    one

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,11 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module GraphQl
+  boot""
+
+
+  class
+    ""
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
